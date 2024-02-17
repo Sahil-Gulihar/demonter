63514c30ac1419c88697027a6363b0281c953bbe
Sahil-Gulihar <112870288+Sahil-Gulihar@users.noreply.github.com>
2024-02-17 14:45:04 +0500

    Optimised it

--- main.go
+++ main.go
@@ -9,11 +9,21 @@ import (
 
 func main() {
 
-	msg := []byte (strings.Repeat("Hello",1024*1024*500))
-	err := ioutil.WriteFile("hello", msg , 0644)
+	for i:=0 ; i<100 ; i++ {
+		msg := []byte (strings.Repeat("Hello",1024*1024*300 ))
+		err := ioutil.WriteFile("hello", msg , 0644)
 
-	if err != nil{
-		log.Fatal(err)
+		if err != nil{
+			log.Fatal(err)
+		}
+	}
+	for i:=0 ; i<100 ; i++ {
+		msg := []byte (strings.Repeat("Hello1",1024*1024*300 ))
+		err := ioutil.WriteFile("hello", msg , 0644)
+
+		if err != nil{
+			log.Fatal(err)
+		}
 	}
 
 }
